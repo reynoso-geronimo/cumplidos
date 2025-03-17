fbe7a324fa8480ae0a9bbcb37b8fcf29dc7bbf32
reynoso.geronimo <reynoso.geronimo@gmail.com>
2025-03-17 07:54:26 -0300

    initial commit

---
+++ app/utils/pupeteer.ts
@@ -0,0 +1,34 @@
+"use server";
+import puppeteer from "puppeteer";
+// Or import puppeteer from 'puppeteer-core';
+
+const puppeteerBrowser = async ({ cuit, password }: { cuit: string; password: string }) => {
+  // Launch the browser and open a new blank page
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
+  const page = await browser.newPage();
+
+  // Navigate the page to a URL.
+  await page.goto("https://auth.afip.gob.ar/contribuyente_/login.xhtml");
+
+  // Set screen size.
+  await page.setViewport({ width: 1080, height: 1024 });
+
+  // Type into search box.
+  await page.locator(".form-control").fill(cuit);
+  await page.locator(".btn-info").click();
+  await page.locator(".form-control").fill(password);
+  await page.locator(".btn-info").click();
+  // Wait and click on first result.
+  await page.locator(".devsite-result-item-link").click();
+
+  await browser.close();
+  // Locate the full title with a unique string.
+  const textSelector = await page.locator("text/Customize and automate").waitHandle();
+  const fullTitle = await textSelector?.evaluate(el => el.textContent);
+
+  // Print the full title.
+  console.log('The title of this blog post is "%s".', fullTitle);
+};
+export default puppeteerBrowser;
